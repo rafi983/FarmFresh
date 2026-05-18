65f5db1984e864f5291c034bedadeb919a7106be
Rafi Zaman <rafiirfan211@gmail.com>
2026-05-18 14:52:43 +0600

    fix vercel image issue

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   // Move serverComponentsExternalPackages out of experimental for Next.js 15
